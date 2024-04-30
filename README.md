a3e1c3dcff643de41a73efc47c99ea7862ed27ee
Christopher Adams <christopher.m.adams2@gmail.com>
2024-04-29 17:05:29 -0700

    basic logic implemented, see comments in evalRound for error condition

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # rock-paper-scissors-js
 A simple rock paper scissors game written in JavaScript
+
+## Basic Structure
+- function `getComputerChoice` to randomly select rock, paper, or scissors
+- function `getHumanChoice` to prompt for user input
+- variables `humanScore` and `computerScore`
+- function `playRound` to play a single round of rock paper scissors
+- function `game` to play a set number of rounds (default 5)
---
+++ rps.js
@@ -0,0 +1,79 @@
+let humanScore = 0;
+let computerScore = 0;
+let winner;
+const choices = ['rock', 'paper', 'scissors'];
+
+function getComputerChoice() {
+    //Math.Random returns a number between 0 and 1;
+    //Math.floor * 3 will then return 0, 1, or 2) which can be used for the array position;
+    let computerChoice = Math.floor(Math.random() * 3);
+    computerChoice = choices[computerChoice];
+    return computerChoice;
+}
+
+function getHumanChoice() {
+    let humanChoice = prompt("Choose One: Rock, Paper, Scissors");
+    //force user input to be a string
+    humanChoice = String(humanChoice);
+    humanChoice = humanChoice.toLocaleLowerCase();
+    if (choices.includes(humanChoice)) {
+        return humanChoice;
+    } else {
+        console.log("You must choose rock, paper, or scissors!");
+    }
+}
+
+function evalRound(humanChoice, computerChoice) {
+    if ((computerChoice === 'rock' && humanChoice === 'paper') ||
+    (computerChoice === 'paper' && humanChoice === 'scissors') ||
+    (computerChoice === 'scissors' && humanChoice === 'rock')) {
+        humanScore++;
+        winner = 1;
+        return winner;
+    } else if ((computerChoice === 'rock' && humanChoice === 'scissors') ||
+    (computerChoice === 'paper' && humanChoice === 'rock') ||
+    (computerChoice === 'scissors' && humanChoice === 'paper')) {
+        computerScore++;
+        winner = 0;
+        return winner;
+    //Add else if to compare draw and then use human choice undefined to count as a loss
+    } else {
+        winner = 2;
+        return winner
+    }
+}
+
+function playRound() {
+    let cChoice = getComputerChoice();
+    let hChoice = getHumanChoice();
+    evalRound(hChoice, cChoice);
+    console.log("Your choice was: " + hChoice);
+    console.log("Computer's choice was: " + cChoice);
+    if (winner === 1) {
+        console.log("You Won!");
+    } else if (winner === 0) {
+        console.log("You Lost!")
+    } else {
+        console.log ("Draw!");
+    }
+}
+
+function game() {
+    for (let i = 1; i <= 5; i++) {
+        playRound();
+        console.log("Your Score: " + humanScore);
+        console.log("Computer's Score: " + computerScore);
+    }
+    
+    if (humanScore > computerScore) {
+        console.log("You're the overall winner!");
+        return;
+    } else if (computerScore > humanScore) {
+        console.log("The computer is the overall winner!");
+        return;
+    } else {
+        console.log("It's a draw!")
+    }
+}
+
+game();
